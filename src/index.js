2ebaa5372b18fc1c977a220800a4b36f9c4cb55b
anilceyhanyeni <anilceyhanyeni@gmail.com>
2023-03-12 17:10:16 +0300

    feat: css ekleme yolları gösterildi.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <section>
-    <p>App 1</p>
-  </section>    
+  <section style={ {border:"3px solid black", padding: "1rem" } } className="container">
+    <p className="kirmizi-yazi">App 1</p>
+  </section>
 );
 
